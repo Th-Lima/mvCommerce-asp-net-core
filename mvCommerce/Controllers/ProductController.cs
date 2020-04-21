fd06b896da7ed64da04a1a7370e236bd78561216
Thales Lima <lthales53@gmail.com>
2020-04-21 20:23:13 -0300

    First commit

---
+++ mvCommerce/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using mvCommerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvCommerce.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ActionResult Visualize()
+        {
+           Product product =  GetProduct();
+
+
+            return View(product);
+        }
+        private Product GetProduct()
+        {
+            return new Product()
+            {
+                Id = 1,
+                Name = "TV",
+                Price = 1500.00,
+                Description = "LED 40 POLEGADAS"
+            };
+        }
+    }
+}
