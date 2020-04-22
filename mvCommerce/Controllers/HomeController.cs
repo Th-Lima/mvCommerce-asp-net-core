097b8b2f384d48a2d3fd6b60ca6d921fa6547ac9
Thales Lima <lthales53@gmail.com>
2020-04-21 21:39:00 -0300

    Configuring the email send

--- mvCommerce/Controllers/HomeController.cs
+++ mvCommerce/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using mvCommerce.Libraries.Email;
+using mvCommerce.Models;
 
 namespace mvCommerce.Controllers
 {
@@ -14,7 +16,14 @@ namespace mvCommerce.Controllers
         }
         public IActionResult ContactAction()
         {
-            return new ContentResult() { Content = "Dados recebidos com sucesso" };
+            Contact contact = new Contact();
+            contact.Name = HttpContext.Request.Form["name"];
+            contact.Email = HttpContext.Request.Form["email"];
+            contact.Text = HttpContext.Request.Form["text"];
+
+            ContactEmail.SendContactPerEmail(contact);
+
+            return new ContentResult() { Content = string.Format("Dados recebidos com sucesso!<br/> Nome: {0} <br/>E-mail: {1} <br/>Texto: {2}", contact.Name, contact.Email, contact.Text), ContentType = "text/html"};
         }
         public IActionResult Contact()
         {
@@ -24,7 +33,7 @@ namespace mvCommerce.Controllers
         {
             return View();
         }
-        public IActionResult RegisterClient()
+        public IActionResult RegisterClient() 
         {
             return View();
         }
