8114a1c82e6cecee5d9228b294fd6a59e024ddf9
Thales Lima <lthales53@gmail.com>
2020-05-01 10:27:46 -0300

    Modifying the database connection to use appsettings.json

--- mvCommerce/Startup.cs
+++ mvCommerce/Startup.cs
@@ -36,7 +36,8 @@ namespace mvCommerce
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-            services.AddDbContext<mvCommerceContext>(options => options.UseSqlServer("connectionString"));
+
+            services.AddDbContext<mvCommerceContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
