757ec062a6b62ba42f146b0d05b37c3923c9ff7c
Thales Lima <lthales53@gmail.com>
2020-05-04 19:27:33 -0300

    Register Client - Repository

---
+++ mvCommerce/Repositories/ClientRepository.cs
@@ -0,0 +1,48 @@
+﻿using mvCommerce.Database;
+using mvCommerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvCommerce.Repositories
+{
+    public class ClientRepository : IClientRepository
+    {
+       private mvCommerceContext _database;
+       public ClientRepository(mvCommerceContext database)
+       {
+            _database = database;
+       }
+        public Client Login(string email, string password)
+        {
+            Client client = _database.Client.Where(c => c.Email == email && c.Password == password).First();
+            return client;
+        }
+       
+        public void Register(Client client)
+        {
+            _database.Add(client);
+            _database.SaveChanges();
+        }
+        public void Update(Client client)
+        {
+            _database.Update(client);
+            _database.SaveChanges();
+        }
+        public void Delete(int id)
+        {
+            Client client = GetClient(id);
+            _database.Remove(client);
+            _database.SaveChanges();
+        }
+        public Client GetClient(int id)
+        {
+            return _database.Client.Find(id);
+        }
+        public List<Client> GetAllClients()
+        {
+            return _database.Client.ToList();
+        }
+    }
+}
