8f7fe61b078da5a06d8b20f76b51ea1873f40d72
Thales Lima <lthales53@gmail.com>
2020-05-13 20:14:05 -0300

    Area - Action Login

--- mvCommerce/Areas/Collaborator/Controllers/HomeController.cs
+++ mvCommerce/Areas/Collaborator/Controllers/HomeController.cs
@@ -1,15 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using mvCommerce.Libraries.Login;
+using mvCommerce.Repositories.Contracts;
 
 namespace mvCommerce.Areas.Collaborator.Controllers
 {
     [Area("Collaborator")]
     public class HomeController : Controller
     {
+        private ICollaboratorRepository _collaboratorRepository;
+        private CollaboratorLogin _collaboratorLogin;
+        public HomeController(ICollaboratorRepository collaboratorRepository, CollaboratorLogin collaboratorLogin)
+        {
+            _collaboratorRepository = collaboratorRepository;
+            _collaboratorLogin = collaboratorLogin;
+        }
+
+        [HttpGet]
         public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login([FromForm]Models.Collaborator collaborator)
+        {
+            Models.Collaborator collaboratorDB = _collaboratorRepository.Login(collaborator.Email, collaborator.Password);
+            if (collaboratorDB != null)
+            {
+                _collaboratorLogin.Login(collaboratorDB);
+                return new RedirectResult(Url.Action(nameof(Panel)));
+
+            }
+            else
+            {
+                ViewData["MSG_E"] = "Algo deu errado, verifique seu email e a senha corretamente!";
+                return View();
+
+            }
+        }
+        
+        public IActionResult Panel()
         {
             return View();
         }
