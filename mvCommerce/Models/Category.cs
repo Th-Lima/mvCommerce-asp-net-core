5b63a5dec8301e8b74774674393c23bf0a2c8584
Thales Lima <lthales53@gmail.com>
2020-05-17 09:32:27 -0300

    Constructing model category

---
+++ mvCommerce/Models/Category.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvCommerce.Models
+{
+    public class Category
+    {
+        public int Id{ get; set; }
+        public string Name { get; set; }
+        /*
+         * www.mvCommerce.com.br/category/1
+         * EX -> www.mvCommerce.com.br/category/toys -> friendly url with slug
+         */
+        public string Slug { get; set; }
+
+        /*
+         * Self relantionship
+         * Ex: 
+         * - 1- Computing - father: null
+         * -- 2- Mouse - father: 1
+         * --- 3- Mouse Gamer - father: 2
+         * ---- 4- Wireless Mouse - father: 2 
+         */
+        public int? CategoryFatherId { get; set; }
+
+        /*
+         * ORM - EntityFrameworkCore
+         */
+         [ForeignKey("CategoryFather")]
+         public virtual Category CategoryFather { get; set; }
+    }
+}
