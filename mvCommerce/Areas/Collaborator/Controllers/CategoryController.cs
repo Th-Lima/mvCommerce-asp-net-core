189993d2744d0bbf5dd6da9a3df3c36447a55166
Thales Lima <lthales53@gmail.com>
2020-05-18 18:37:26 -0300

    Add Category controller and fix view bugs

---
+++ mvCommerce/Areas/Collaborator/Controllers/CategoryController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using mvCommerce.Libraries.Filter;
+using mvCommerce.Models;
+using mvCommerce.Repositories.Contracts;
+
+namespace mvCommerce.Areas.Collaborator.Controllers
+{
+    [Area("Collaborator")]
+    //TODO - Enable Authorization
+   //[CollaboratorAuthorization]
+    public class CategoryController : Controller
+    {
+        private ICategoryRepository _categoryRepository;
+        public CategoryController(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+        public IActionResult Index()
+        {
+            List<Category> categories = _categoryRepository.GetAllCategories().ToList();
+            return View(categories);
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register([FromForm]Category category)
+        {
+            //TODO - Implement
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Update()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Update([FromForm]Category category)
+        {
+            //TODO - Implement
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            return View();
+        }
+    }
+}
