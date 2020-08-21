9f1dbe6b392e67432a59c8b1398e30b8da7a08ee
Thales Lima <lthales53@gmail.com>
2020-08-21 15:28:35 -0300

    Implementing imageController and file manager

---
+++ mvCommerce/Areas/Collaborator/Controllers/ImageController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using mvCommerce.Libraries.Files;
+
+namespace mvCommerce.Areas.Collaborator.Controllers
+{
+    [Area("Collaborator")]
+    public class ImageController : Controller
+    {
+        public IActionResult Storage(IFormFile file)
+        {
+           var path = FileManager.RegisterProductImage(file);
+            if(path.Length > 0)
+            {
+                return Ok(new { path = path });
+            }
+            else
+            {
+                return new StatusCodeResult(500);
+            }
+        }
+        public IActionResult Delete(string path)
+        {
+            if(FileManager.DeleteProductImage(path))
+            {
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ mvCommerce/Libraries/Files/FileManager.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.IO;
+
+namespace mvCommerce.Libraries.Files
+{
+    public class FileManager
+    {
+        public static string RegisterProductImage(IFormFile file)
+        {
+            var fileName = Path.GetFileName(file.FileName);
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/uploads/temp", fileName);
+
+            using(var stream = new FileStream(path, FileMode.Create))
+            {
+                file.CopyTo(stream);
+            }
+            return Path.Combine("/uploads/temp", fileName);
+        }
+        public static bool DeleteProductImage(string path)
+        {
+            string pathFile = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", path.TrimStart('/'));
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
