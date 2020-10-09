c6d9cb985c77c2695f1ed78c7e3a8b4380cae61b
Thales Lima <lthales53@gmail.com>
2020-10-09 18:23:54 -0300

    Implementing algorithm of package

---
+++ mvCommerce/Libraries/Manager/Freight/CalculatePackage.cs
@@ -0,0 +1,42 @@
+﻿using mvCommerce.Models;
+using mvCommerce.Models.ProductAggregator;
+using System.Collections.Generic;
+
+namespace mvCommerce.Libraries.Manager.Freight
+{
+    public class CalculatePackage
+    {
+        public List<Package> CalculatingPackage(List<ProductItem> products)
+        {
+            List<Package> packages = new List<Package>();
+
+            Package package = new Package();
+            foreach(var product in products)
+            {
+                for(int i = 0; i < product.AmountProductsCart; i++)
+                {
+                    var weight = package.Weight + product.Weight;
+                    var length = (package.Length > product.Length) ? package.Length : product.Length;
+                    var width = (package.Width > product.Width) ? package.Width : product.Width; ;
+                    var height = package.Height + product.Height;
+                    var dimension = length + width + height;
+
+                    if(weight > 30 || dimension > 200)
+                    {
+                        packages.Add(package);
+                        
+                        package = new Package();
+                    }
+
+                   package.Weight += product.Weight;
+                   package.Length = (package.Length > product.Length) ? package.Length : product.Length;
+                   package.Width = (package.Width > product.Width) ? package.Width : product.Width; ;
+                   package.Height += product.Height;
+                }
+            }
+            packages.Add(package);
+
+            return packages;
+        }  
+    }
+}
---
+++ mvCommerce/Models/Package.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvCommerce.Models
+{
+    public class Package
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int Length { get; set; }
+        public double Weight { get; set; }
+    }
+}
