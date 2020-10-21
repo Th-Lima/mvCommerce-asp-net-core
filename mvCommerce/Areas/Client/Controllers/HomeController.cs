4206e799db03338d8dcd615d4be65f248bacce13
Thales Lima <lthales53@gmail.com>
2020-10-21 20:45:01 -0300

    Implementing user name on login button

--- mvCommerce/Areas/Client/Controllers/HomeController.cs
+++ mvCommerce/Areas/Client/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using mvCommerce.Libraries.Filter;
 using mvCommerce.Libraries.Login;
 using mvCommerce.Repositories.Contracts;
